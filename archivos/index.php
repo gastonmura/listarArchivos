897071e125b6216607999bb057aee5de4f537660
gastonmura <gastonmura@hotmail.com>
2022-03-23 13:25:30 -0300

    Add files via upload

--- archivos/index.php
+++ archivos/index.php
@@ -1,1 +1,37 @@
+<?php
 
+echo '
+{
+"total": 27,
+"archivos" : [
+{"nombre": "BCH-Bantotal-Prueba Ticket 9216-CP01-Toma archivo ej3 .doc", "path": "c:\\Users\\gmura\\Downloads", "tipo": "doc", "size": 205},
+{"nombre": "BCH-Bantotal-Prueba Ticket 9216-CP01-Toma archivo ej5.doc", "path": "c:\\Users\\gmura\\Downloads", "tipo": "doc", "size": 709},
+{"nombre": "30.GAM_API_sp.pdf", "path": "c:\\Users\\gmura\\Downloads", "tipo": "pdf", "size": 1045},
+{"nombre": "ACT001_RTE_05032018_BCKP_MI_KB.txt .doc", "path": "c:\\Users\\gmura\\Downloads", "tipo": "txt", "size": 3},
+{"nombre": "ExportWWSolicitud-1732.xlsx", "path": "c:\\Users\\gmura\\Downloads", "tipo": "xlsx", "size": 4},
+{"nombre": "Recibos de Sueldo027150072801255.pdf", "path": "c:\\Users\\gmura\\Downloads", "tipo": "pdf", "size": 339},
+{"nombre": "PracticalExercises-GeneXus16_Analyst_sp (1).pdf", "path": "c:\\Users\\gmura\\Downloads", "tipo": "pdf", "size": 3695},
+{"nombre": "Solicitud-de-canje-maramillas.png", "path": "c:\\Users\\gmura\\Downloads", "tipo": "png", "size": 97},
+{"nombre": "BCH-Bantotal-Prueba Ticket 9216-CP01-Toma archivo ej3 .doc", "path": "c:\\Users\\gmura\\Downloads", "tipo": "doc", "size": 205},
+{"nombre": "BCH-Bantotal-Prueba Ticket 9216-CP01-Toma archivo ej5.doc", "path": "c:\\Users\\gmura\\Downloads", "tipo": "doc", "size": 709},
+{"nombre": "30.GAM_API_sp.pdf", "path": "c:\\Users\\gmura\\Downloads", "tipo": "pdf", "size": 1045},
+{"nombre": "ACT001_RTE_05032018_BCKP_MI_KB.txt .doc", "path": "c:\\Users\\gmura\\Downloads", "tipo": "txt", "size": 3},
+{"nombre": "ExportWWSolicitud-1732.xlsx", "path": "c:\\Users\\gmura\\Downloads", "tipo": "xlsx", "size": 4},
+{"nombre": "Recibos de Sueldo027150072801255.pdf", "path": "c:\\Users\\gmura\\Downloads", "tipo": "pdf", "size": 339},
+{"nombre": "PracticalExercises-GeneXus16_Analyst_sp (1).pdf", "path": "c:\\Users\\gmura\\Downloads", "tipo": "pdf", "size": 3695},
+{"nombre": "Solicitud-de-canje-maramillas.png", "path": "c:\\Users\\gmura\\Downloads", "tipo": "png", "size": 97},
+{"nombre": "BCH-Bantotal-Prueba Ticket 9216-CP01-Toma archivo ej3 .doc", "path": "c:\\Users\\gmura\\Downloads", "tipo": "doc", "size": 205},
+{"nombre": "BCH-Bantotal-Prueba Ticket 9216-CP01-Toma archivo ej5.doc", "path": "c:\\Users\\gmura\\Downloads", "tipo": "doc", "size": 709},
+{"nombre": "30.GAM_API_sp.pdf", "path": "c:\\Users\\gmura\\Downloads", "tipo": "pdf", "size": 1045},
+{"nombre": "ACT001_RTE_05032018_BCKP_MI_KB.txt .doc", "path": "c:\\Users\\gmura\\Downloads", "tipo": "txt", "size": 3},
+{"nombre": "ExportWWSolicitud-1732.xlsx", "path": "c:\\Users\\gmura\\Downloads", "tipo": "xlsx", "size": 4},
+{"nombre": "Recibos de Sueldo027150072801255.pdf", "path": "c:\\Users\\gmura\\Downloads", "tipo": "pdf", "size": 339},
+{"nombre": "PracticalExercises-GeneXus16_Analyst_sp (1).pdf", "path": "c:\\Users\\gmura\\Downloads", "tipo": "pdf", "size": 3695},
+{"nombre": "Solicitud-de-canje-maramillas.png", "path": "c:\\Users\\gmura\\Downloads", "tipo": "png", "size": 97},
+{"nombre": "ExportWWSolicitud-1732.xlsx", "path": "c:\\Users\\gmura\\Downloads", "tipo": "xlsx", "size": 4},
+{"nombre": "Recibos de Sueldo027150072801255.pdf", "path": "c:\\Users\\gmura\\Downloads", "tipo": "pdf", "size": 339},
+{"nombre": "PracticalExercises-GeneXus16_Analyst_sp (1).pdf", "path": "c:\\Users\\gmura\\Downloads", "tipo": "pdf", "size": 3695}
+]
+}
+';
+?>
